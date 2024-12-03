bb185e40277a2626978c9f1bbd38ec096a1c966d
vladamaks <vladamaks@gmail.com>
2024-12-03 18:31:41 +0100

    Displaying genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,20 +1,32 @@
+import { HStack, Image, List, ListItem, Stack, Text } from "@chakra-ui/react";
 import { useGenres } from "../hooks/useGenres";
+import getCroppedImageURL from "../services/image-url";
 
 export interface Genre {
   id: number;
   name: string;
   slug: string;
+  image_background: string;
 }
 
 const GenreList = () => {
   const { data } = useGenres();
 
   return (
-    <ul>
+    <List>
       {data.map((genre) => (
-        <li key={genre.id}>{genre.name}</li>
+        <ListItem key={genre.id} paddingY="5px">
+          <HStack>
+            <Image
+              src={getCroppedImageURL(genre.image_background)}
+              boxSize="32px"
+              borderRadius={8}
+            />
+            <Text>{genre.name}</Text>
+          </HStack>
+        </ListItem>
       ))}
-    </ul>
+    </List>
   );
 };
 
