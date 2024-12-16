4d273e3225fe8dd6253059e62121f0bee337f243
vladamaks <vladamaks@gmail.com>
2024-12-16 14:34:28 -0600

    Ship platforms with the app

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,6 +1,7 @@
-import { Platform } from "../components/GameGrid";
-import useData from "./useData";
+import platforms from "../data/platforms";
 
-const usePlatforms = () => useData<Platform>("/platforms/lists/parents");
+const usePlatforms = () => {
+  return { data: platforms, isLoading: false, error: null };
+};
 
 export default usePlatforms;
