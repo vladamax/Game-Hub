e21d1fbdb76fdcd5e7e962c7306f19b4ee40daa8
vladamaks <vladamaks@gmail.com>
2024-12-23 20:45:56 -0600

    Removed isFetchingNextPage from InfiniteScrolling -  unused

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -12,14 +12,8 @@ interface Props {
 }
 
 export const GameGrid = ({ gameQuery }: Props) => {
-  const {
-    data,
-    error,
-    isLoading,
-    isFetchingNextPage,
-    fetchNextPage,
-    hasNextPage,
-  } = useGames(gameQuery);
+  const { data, error, isLoading, fetchNextPage, hasNextPage } =
+    useGames(gameQuery);
 
   const skeletons = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
