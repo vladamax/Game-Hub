2d8b0a7aa89cc7b74b6eb2f0d55553bc12297ff4
vladamaks <vladamaks@gmail.com>
2024-12-31 09:53:08 -0600

    Build game screenshots

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -4,6 +4,7 @@ import { ExpandableText } from "../components/ExpandableText";
 import useGame from "../hooks/useGame";
 import { GameAttributes } from "../components/GameAttributes";
 import { GameTrailer } from "../components/GameTrailer";
+import { GameScreenshots } from "../components/GameScreenshots";
 
 export const GameDetail = () => {
   const { slug } = useParams();
@@ -19,6 +20,7 @@ export const GameDetail = () => {
       <ExpandableText>{game.description_raw}</ExpandableText>
       <GameAttributes game={game} />
       <GameTrailer gameId={game.id} />
+      <GameScreenshots gameId={game.id} />
     </>
   );
 };
